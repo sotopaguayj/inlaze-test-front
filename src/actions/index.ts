0e1a8916b2b56798891f80345c4e52b9e53ee38d
sotopaguayj <sotopaguayj@gmail.com>
2024-09-20 14:56:52 -0500

    remove manual movie api url

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const MovieAPI = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL || "https://api.themoviedb.org/3"}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
   },
